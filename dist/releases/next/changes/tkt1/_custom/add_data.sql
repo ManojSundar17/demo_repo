8a8fb0a178ab25df863b03c16db4e1c31f5b69d3
Manoj <manoj.sundar@oracle.com>
2025-10-23 21:23:36 +0530

    manual scripts

---
+++ dist/releases/next/changes/tkt1/_custom/add_data.sql
@@ -0,0 +1,7 @@
+-- liquibase formatted sql
+-- changeset  SqlCl:1761234756116 stripComments:false logicalFilePath:tkt1/_custom/add_data.sql
+-- sqlcl_snapshot dist/releases/next/changes/tkt1/_custom/add_data.sql:null:null:custom
+
+
+
+insert into wksp_sqlcl.demo_table1 (id) values (1); 
