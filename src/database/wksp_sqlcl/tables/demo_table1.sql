f0a53c3a045ddb630ff5999c91f54bbcaadeaeff
Manoj <manoj.sundar@oracle.com>
2025-10-23 21:21:51 +0530

    changes

---
+++ src/database/wksp_sqlcl/tables/demo_table1.sql
@@ -0,0 +1,6 @@
+create table wksp_sqlcl.demo_table1 (
+    id number
+);
+
+
+-- sqlcl_snapshot {"hash":"2d7f92b5b7c901b4ef6c0922952b7d0fcc72272e","type":"TABLE","name":"DEMO_TABLE1","schemaName":"WKSP_SQLCL","sxml":"\n  <TABLE xmlns=\"http://xmlns.oracle.com/ku\" version=\"1.0\">\n   <SCHEMA>WKSP_SQLCL</SCHEMA>\n   <NAME>DEMO_TABLE1</NAME>\n   <RELATIONAL_TABLE>\n      <COL_LIST>\n         <COL_LIST_ITEM>\n            <NAME>ID</NAME>\n            <DATATYPE>NUMBER</DATATYPE>\n            \n         </COL_LIST_ITEM>\n      </COL_LIST>\n      <DEFAULT_COLLATION>USING_NLS_COMP</DEFAULT_COLLATION>\n      <PHYSICAL_PROPERTIES>\n         <HEAP_TABLE></HEAP_TABLE>\n      </PHYSICAL_PROPERTIES>\n      \n   </RELATIONAL_TABLE>\n</TABLE>"}
--- src/database/wksp_sqlcl/tables/manoj_oct_22.sql
+++ src/database/wksp_sqlcl/tables/manoj_oct_22.sql
@@ -1,7 +1,8 @@
 create table wksp_sqlcl.manoj_oct_22 (
-    id   number not null enable,
-    name varchar2(10 byte)
+    id        number not null enable,
+    name      varchar2(10 byte),
+    value_col varchar2(100 byte)
 );
 
 
--- sqlcl_snapshot {"hash":"92a788806f68bb1535311dddbf02b8f11444956e","type":"TABLE","name":"MANOJ_OCT_22","schemaName":"WKSP_SQLCL","sxml":"\n  <TABLE xmlns=\"http://xmlns.oracle.com/ku\" version=\"1.0\">\n   <SCHEMA>WKSP_SQLCL</SCHEMA>\n   <NAME>MANOJ_OCT_22</NAME>\n   <RELATIONAL_TABLE>\n      <COL_LIST>\n         <COL_LIST_ITEM>\n            <NAME>ID</NAME>\n            <DATATYPE>NUMBER</DATATYPE>\n            <NOT_NULL></NOT_NULL>\n            \n         </COL_LIST_ITEM>\n         <COL_LIST_ITEM>\n            <NAME>NAME</NAME>\n            <DATATYPE>VARCHAR2</DATATYPE>\n            <LENGTH>10</LENGTH>\n            <COLLATE_NAME>USING_NLS_COMP</COLLATE_NAME>\n            \n         </COL_LIST_ITEM>\n      </COL_LIST>\n      <DEFAULT_COLLATION>USING_NLS_COMP</DEFAULT_COLLATION>\n      <PHYSICAL_PROPERTIES>\n         <HEAP_TABLE></HEAP_TABLE>\n      </PHYSICAL_PROPERTIES>\n      \n   </RELATIONAL_TABLE>\n</TABLE>"}
+-- sqlcl_snapshot {"hash":"63ae2c1a27caf7ee0c6ea0b8817d095754417352","type":"TABLE","name":"MANOJ_OCT_22","schemaName":"WKSP_SQLCL","sxml":"\n  <TABLE xmlns=\"http://xmlns.oracle.com/ku\" version=\"1.0\">\n   <SCHEMA>WKSP_SQLCL</SCHEMA>\n   <NAME>MANOJ_OCT_22</NAME>\n   <RELATIONAL_TABLE>\n      <COL_LIST>\n         <COL_LIST_ITEM>\n            <NAME>ID</NAME>\n            <DATATYPE>NUMBER</DATATYPE>\n            <NOT_NULL></NOT_NULL>\n            \n         </COL_LIST_ITEM>\n         <COL_LIST_ITEM>\n            <NAME>NAME</NAME>\n            <DATATYPE>VARCHAR2</DATATYPE>\n            <LENGTH>10</LENGTH>\n            <COLLATE_NAME>USING_NLS_COMP</COLLATE_NAME>\n            \n         </COL_LIST_ITEM>\n         <COL_LIST_ITEM>\n            <NAME>VALUE_COL</NAME>\n            <DATATYPE>VARCHAR2</DATATYPE>\n            <LENGTH>100</LENGTH>\n            <COLLATE_NAME>USING_NLS_COMP</COLLATE_NAME>\n            \n         </COL_LIST_ITEM>\n      </COL_LIST>\n      <DEFAULT_COLLATION>USING_NLS_COMP</DEFAULT_COLLATION>\n      <PHYSICAL_PROPERTIES>\n         <HEAP_TABLE></HEAP_TABLE>\n      </PHYSICAL_PROPERTIES>\n      \n   </RELATIONAL_TABLE>\n</TABLE>"}
